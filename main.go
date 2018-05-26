f4b9422eeb334179f3951d3febc8444b49177361
yappps <yapyuanxin@gmail.com>
2018-05-26 17:24:36 +0800

    added Regexp to validate path.

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"errors"
 	"html/template"
 	"log"
 	"net/http"
+	"regexp"
 
 	"github.com/yappps/LearnGo-Top-Ten-Words/myLibraries"
 )
+
+// var validPath = regexp.MustCompile(`^(\/|\/results\/\?userInput=([a-zA-Z0-9+%]*))$`)
+var validPath = regexp.MustCompile(`^(\/|\/results\/)$`)
+
+func validatePath(w http.ResponseWriter, r *http.Request) error {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if m == nil {
+		http.NotFound(w, r)
+		return errors.New("Invalid Page")
+	}
+	return nil 
+}
 
 var templates = template.Must(template.ParseFiles("templates/index.html", "templates/results.html"))
 
@@ -18,9 +32,17 @@ func renderTemplate(w http.ResponseWriter, tmpl string, c *[]topten.Category) {
 	}
 }
 func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	err := validatePath(w, r)
+	if err != nil {
+		return
+	}
 	renderTemplate(w, "index", nil)
 }
 func resultPageHandler(w http.ResponseWriter, r *http.Request) {
+	err := validatePath(w, r)
+	if err != nil {
+		return
+	}
 	input := r.FormValue("userInput")
 	results := topten.GetTopTenWords(&input)
 	renderTemplate(w, "results", &results)
