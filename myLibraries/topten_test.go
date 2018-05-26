49738a861597394633dfe57e0a45886863e77a17
yappps <yapyuanxin@gmail.com>
2018-05-26 19:32:22 +0800

    added test for TabulateWords

--- myLibraries/topten_test.go
+++ myLibraries/topten_test.go
@@ -5,12 +5,28 @@ import (
 )
 
 func TestConvertToSlice(t *testing.T) {
-	input := "Unit testing, (in) !Go is just"
+	str := "Unit testing, (in) !Go is just"
 	exp := []string{"unit", "testing", "in", "go", "is", "just"}
-	for index, value := range ConvertToSlice(&input) {
+	for index, value := range ConvertToSlice(&str) {
 		if value != exp[index] {
 			t.Errorf("Expected %s to equal %s", value, exp[index])
 		}
 	}
 }
 
+func TestTabulateWords(t *testing.T) {
+	input := []string{"hi", "hi", "bye", "bye", "what", "the", "the", "test", "k"}
+	exp := map[string]int{
+		"hi":   2,
+		"bye":  2,
+		"the":  2,
+		"what": 1,
+		"test": 1,
+		"k":    1,
+	}
+	for key, value := range TabulateWords(&input) {
+		if value != exp[key] {
+			t.Errorf("Expected %d to equal %d", value, exp[key])
+		}
+	}
+}
